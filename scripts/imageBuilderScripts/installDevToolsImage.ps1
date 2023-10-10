80761d539e5de5020117b0b6e6bb6aec362f3faf
Ayoub KEBAILI <keayoub@msn.com>
2023-10-10 12:00:05 -0400

    update script for WSL2 support

--- scripts/imageBuilderScripts/installDevToolsImage.ps1
+++ scripts/imageBuilderScripts/installDevToolsImage.ps1
@@ -31,6 +31,16 @@ Write-Host "All Packages Installed"
 try {
     # Clone Application from azure    
     Write-Host "Install wsl to run docker linux container"
+
+    # Enable the Windows Subsystem for Linux optional feature    
+    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+    # Enable the Virtual Machine Platform optional feature 
+    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+    wsl.exe --update
+    # Download and install the Linux kernel update package
+    wsl --set-default-version 2
+    # Download the Linux kernel update package
     wsl --install -d ubuntu
     RefreshEnv.cmd
 
